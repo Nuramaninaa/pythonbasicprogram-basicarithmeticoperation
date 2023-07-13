ce58b705efdbb3424223e8e6a2659b9de40cc6d9
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-13 17:24:49 +0800

    Add files via upload

---
+++ pythonbasicarithmeticoperation.py
@@ -0,0 +1,25 @@
+#python program to perform basic arithmetic operation (+ , - , ÷, *)
+
+#initialize num1
+num1 = input ("Enter value of num1 : ")
+
+#initialize num2
+num2 = input ("Enter value of num2 : ")
+
+#formula of sum
+sum = float(num1) + float(num2)
+
+#formula of subtract
+sub = float(num1) - float(num2)
+
+#formula of division
+div = float(num1) / float(num2)
+
+#formula of multiplication
+mul = float(num1) * float(num2)
+
+#print output all arithmetic operation
+print ("total sum is ", sum)
+print ("total sub is ", sub)
+print ("total division is ", div)
+print ("total multiplication is ", mul)
